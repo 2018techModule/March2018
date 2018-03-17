9302a5b7a3eb642275c4fba166c5cb50244e4a9a
Maksim Antov <max_man@abv.bg>
2018-03-17 21:59:21 +0200

    Added solution for Problem 1.

---
+++ DebitCardNumber/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DebitCardNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+            int lastNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{firstNumber:D4} {secondNumber:D4} {thirdNumber:D4} {lastNumber:D4}");
+        }
+    }
+}
