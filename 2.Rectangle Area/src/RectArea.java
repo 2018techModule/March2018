0d3c6f97654267d61c34bd85bd3d0f1fb6a66642
Vladimir Simeonov <vladimir.simeonov.1995@gmail.com>
2018-03-19 09:41:57 +0200

    add solution for Problem 2

---
+++ 2.Rectangle Area/src/RectArea.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class RectArea {
+
+	public static void main(String[] args) throws IOException  {
+		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+		double width = Double.parseDouble(input.readLine());
+		double height = Double.parseDouble(input.readLine());
+		
+		System.out.printf("%.2f", width*height);
+		
+	}
+
+}
