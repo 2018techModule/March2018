418ee451b3e59d2a6f94935b53059fef9fac8130
Maksim Antov <max_man@abv.bg>
2018-03-22 15:32:50 +0200

    Renamed Problem's folder, documented Problem 2, added Description to Problem 3, changed folder structure.

--- Exercises/2.Rectangle Area/src/RectArea.java
+++ Exercises/2.Rectangle Area/src/RectArea.java
@@ -5,10 +5,12 @@ import java.io.InputStreamReader;
 public class RectArea {
 
 	public static void main(String[] args) throws IOException  {
-		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+		//Start reading from the console: Rectangle's width and rectangle's height.
+                BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 		double width = Double.parseDouble(input.readLine());
 		double height = Double.parseDouble(input.readLine());
 		
+                //Calculating Rectangle's Area.
 		System.out.printf("%.2f", width*height);
 		
 	}
