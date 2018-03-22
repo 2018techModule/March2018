d7d341caa9527efb0a0d9e4b737f8677905fbdc1
Val Tod <valtodus@gmail.com>
2018-03-22 12:26:50 +0200

    ValTod documented the code of Solution of Problem 1

--- DebitCardNumber/Program.cs
+++ DebitCardNumber/Program.cs
@@ -6,11 +6,15 @@ namespace DebitCardNumber
     {
         static void Main(string[] args)
         {
+            //input the first part of number
             int firstNumber = int.Parse(Console.ReadLine());
+            //input the second part of number
             int secondNumber = int.Parse(Console.ReadLine());
+            //input the third part of number
             int thirdNumber = int.Parse(Console.ReadLine());
+            //input the fourth part of number
             int lastNumber = int.Parse(Console.ReadLine());
-
+            //write the number in the console
             Console.WriteLine($"{firstNumber:D4} {secondNumber:D4} {thirdNumber:D4} {lastNumber:D4}");
         }
     }
