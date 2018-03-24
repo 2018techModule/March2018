9980e6b37b681271318f41eeabb9d3386796246e
Vladimir Simeonov <vladimir.simeonov.1995@gmail.com>
2018-03-24 18:18:39 +0200

    changes variable name in Problem 2

--- Exercises/2.Rectangle Area/src/RectArea.java
+++ Exercises/2.Rectangle Area/src/RectArea.java
@@ -7,11 +7,11 @@ public class RectArea {
 	public static void main(String[] args) throws IOException  {
 		//Start reading from the console: Rectangle's width and rectangle's height.
                 BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-		double width = Double.parseDouble(input.readLine());
+		double rectWidth = Double.parseDouble(input.readLine());
 		double height = Double.parseDouble(input.readLine());
 		
                 //Calculating Rectangle's Area.
-		System.out.printf("%.2f", width*height);
+		System.out.printf("%.2f", rectWidth*height);
 		
 	}
 
