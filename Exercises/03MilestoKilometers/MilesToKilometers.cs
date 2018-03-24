6a30d8b408a591215bca904c4104aff52ce0ad31
Vladimir Simeonov <vladimir.simeonov.1995@gmail.com>
2018-03-24 11:58:44 +0200

    documents the code of Solution of Problem 3

--- Exercises/03MilestoKilometers/MilesToKilometers.cs
+++ Exercises/03MilestoKilometers/MilesToKilometers.cs
@@ -2,12 +2,12 @@ using System;
 					
 public class Program
 {
-    public static void Main()
+    public static void Main()   //The main function
     {
-        double mili = double.Parse(Console.ReadLine());
+        double mili = double.Parse(Console.ReadLine());  // We recieve from the Console the miles -> double
         
-        double kilometri = mili*1.60934;
+        double kilometri = mili*1.60934;   // Convert the miles into kilometers -> double to double
 		
-        Console.WriteLine("{0:F2}",kilometri);
-    }
+        Console.WriteLine("{0:F2}",kilometri);  // Print he kilometers on the Console
+    } 
 }
