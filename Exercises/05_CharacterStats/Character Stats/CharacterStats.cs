d0f54e4b5da6b7fce4e4a9553c3106a2f32ebfc5
Vladimir Simeonov <vladimir.simeonov.1995@gmail.com>
2018-03-24 12:19:26 +0200

    changes variable name in Problem 5

--- Exercises/05_CharacterStats/Character Stats/CharacterStats.cs
+++ Exercises/05_CharacterStats/Character Stats/CharacterStats.cs
@@ -7,20 +7,20 @@ namespace Character_Stats
         static void Main(string[] args)
         {
             string name = Console.ReadLine();
-            int a1 = int.Parse(Console.ReadLine());
-            int a2 = int.Parse(Console.ReadLine());
-            int b1 = int.Parse(Console.ReadLine());
-            int b2 = int.Parse(Console.ReadLine());
+            int currentHealth = int.Parse(Console.ReadLine());
+            int maxHealth = int.Parse(Console.ReadLine());
+            int currentEnergy = int.Parse(Console.ReadLine());
+            int maxEnergy = int.Parse(Console.ReadLine());
             Console.WriteLine($"Name: {name}");
-            string health1 = new string('|', a1);
-            string health2 = new string('.', a2 - a1);
-            if (a1 == a2)
+            string health1 = new string('|', currentHealth);
+            string health2 = new string('.', maxHealth - currentHealth);
+            if (currentHealth == maxHealth)
                 Console.WriteLine($"Health: |{health1}|");
             else
                 Console.WriteLine($"Health: |{health1}{health2}|");
-            string energy1 = new string('|', b1);
-            string energy2 = new string('.', b2 - b1);
-            if (b1 == b2)
+            string energy1 = new string('|', currentEnergy);
+            string energy2 = new string('.', maxEnergy - currentEnergy);
+            if (currentEnergy == maxEnergy)
                 Console.WriteLine($"Energy: |{energy1}|");
             else
                 Console.WriteLine($"Energy: |{energy1}{energy2}|");
